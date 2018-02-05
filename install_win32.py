6ff2648d152f7548ce1f4e12ecd5955da3b678b8
Rasmus Toftdahl Olesen (RTOL) <rtol@oticon.com>
2018-02-05 08:32:13 +0100

    Migrate install script for win32 to python3

--- install_win32.py
+++ install_win32.py
@@ -9,8 +9,8 @@ appdata = os.getenv('APPDATA')
 dotemacs = path.dirname(__file__)
 filename = '.emacs'
 
-print 'Home:', home
-print 'Application Data:', appdata
-print 'dotemacs:', dotemacs
+print ( 'Home:', home )
+print ( 'Application Data:', appdata )
+print ( 'dotemacs:', dotemacs )
 
 shutil.copyfile ( path.join(dotemacs, filename), path.join(appdata, filename) )
