c7b49314ee94d4210a0d82d7689f569e09da40ff
Rasmus Franch Toftdahl Olesen <rtol@oticon.com>
2018-03-28 07:33:15 +0200

    Also use symlink when installing on windows.

--- install_win32.py
+++ install_win32.py
@@ -2,7 +2,8 @@
 
 import os
 from os import path
-import shutil
+import sys
+#import shutil
 
 home = path.expanduser ( '~' )
 appdata = os.getenv('APPDATA')
@@ -13,4 +14,15 @@ print ( 'Home:', home )
 print ( 'Application Data:', appdata )
 print ( 'dotemacs:', dotemacs )
 
-shutil.copyfile ( path.join(dotemacs, filename), path.join(appdata, filename) )
+link = path.join(appdata, filename)
+target = path.join(dotemacs, filename)
+
+ret = os.system ( 'NET SESSION >nul 2>&1' )
+if ret != 0:
+    print ( 'This script needs to run as admin' )
+    sys.exit(-1)
+
+if os.system ( f'mklink {link} {target}' ) != 0:
+    print ( f' - File probably already exists, please delete {link} and try again.' )
+
+#shutil.copyfile ( path.join(dotemacs, filename), path.join(appdata, filename) )
